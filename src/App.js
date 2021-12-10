025b4aa31327779c88d3e7df09b4d2213e8ae1df
Dominic <dominicdeantonio@icloud.com>
2021-12-10 10:32:45 -0500

    Implemented some navbar designs

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     
   return (
     <>
-      <NavBar user={user} />
+      <NavBar user={user} loading={loading} />
       {location.pathname === '/' && <HomeView />}
       <Outlet />
     </>
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { logout } from '../../services/firebase';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { signOut } from '../../services/firebase';
 
 function NavBar(props) {
     return (
         <div>
-            <h1>NoBS Jobs</h1>
-            {props.user && <p>Logged in: <Link to="/profile">{props.user?.email}</Link></p>}
-            {!props.user && <Link to="/profile">Profile</Link>}
-            <p>Env: {process.env.REACT_APP_HELLO}</p>
-            <nav
-                style={{
-                    borderBottom: "solid 1px",
-                    paddingBottom: "1rem"
-                }}>
-                {/* <Link className={path === "/create-post" ? 'btn btn-primary' : ''} to="/create-post">Create Post</Link> */}
-                <Link to="/create-post">Create Post</Link><br />
-                <Link to="/favorites">Favorites</Link><br />
-                <Link to="/">Home</Link><br/>
-                <button onClick={logout}>Logout</button>
-                {/* <Link to="/jobs">Jobs</Link>
-                
-                <Link to="/error">Error</Link> */}
-            </nav>
+            <Navbar bg="primary" variant="dark" className='shadow'>
+                <Container>
+                    <Navbar.Brand href="/">NoBS Jobs</Navbar.Brand>
+                    <Nav className="me-auto">
+                        {(props.user || props.loading) && <Nav.Link href="/create-post">Create Post</Nav.Link>}
+                        {(props.user || props.loading) && <Nav.Link href="/favorites">Favorites</Nav.Link>}
+                        {(props.user || props.loading) && <Nav.Link onClick={signOut}>Sign Out</Nav.Link>}
+                        {(!props.user && !props.loading) && <Nav.Link href='/profile'>Sign In</Nav.Link>}
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     );
 
